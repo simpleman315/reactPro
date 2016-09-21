9a757fb206840acc5a1e8b099d648d23f520b7aa
WeiWeiQin <weiweiqin@xiaoyouzi.com>
2016-09-21 10:53:54 +0800

    增加图片加载器

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,42 @@ var webpackConfig = {
         }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.json|\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'file'
+        }, {
+            test: /\.(woff|woff2)$/,
+            loader: 'url?prefix=font/&limit=5000'
+        }, {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=application/octet-stream'
+        }, {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=image/svg+xml'
+        }, {
+            test: /\.gif/,
+            loader: 'url-loader?limit=10000&mimetype=image/gif'
+        }, {
+            test: /\.jpg/,
+            loader: 'url-loader?limit=10000&mimetype=image/jpg'
+        }, {
+            test: /\.png/,
+            exclude: /(images)/,
+            loader: 'url-loader?limit=10000&mimetype=image/png'
+        }, {
+            test: /\.png$/,
+            include: /(images)/,
+            loader: 'file-loader?name=/images/[name].[ext]'
+        }, {
+            test: /\.ico$/,
+            include: /(images)/,
+            loader: 'url-loader?limit=10000&mimetype=image/x-icon'
+        }, {
+            test: /[\/\\]node_modules[\/\\]some-module[\/\\]index\.js$/,
+            loader: 'imports?this=>window'
+        }, {
+            test: /[\/\\]node_modules[\/\\]some-module[\/\\]index\.js$/,
+            loader: 'imports?define=>false'
         }]
     },
     plugins: [
