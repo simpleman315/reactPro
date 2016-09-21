d44dee2844ecbe1bfe0206ac8ed701c142a268c4
WeiWeiQin <weiweiqin@xiaoyouzi.com>
2016-09-21 10:10:58 +0800

    稳定版本1

--- webpack.config.js
+++ webpack.config.js
@@ -4,97 +4,94 @@ var fs = require('fs');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var prod = process.env.NODE_ENV === 'production';
-var webpackConfig  = {
+var webpackConfig = {
     entry: {
-		'bundle':'./public/js/main.js',
-		'test':'./public/js/test.jsx'
-	},
-	
+        'main': './public/js/main.js',
+        'test': './public/js/test.jsx'
+    },
+
     output: {
         path: path.resolve(__dirname, './public/assets/'),
-        filename: '[name].js',
-		publicPath: prod ? '//static.seeyouyima.com/data.tataquan.com/[hash:8]/' : 'http://127.0.0.1:8080/public/assets/'
+        filename: 'js/[name].js',
+        publicPath: prod ? '//static.seeyouyima.com/data.tataquan.com/[hash:8]/' : 'http://127.0.0.1:8080/public/assets/'
     },
-	
-	resolve: {
+
+    resolve: {
         extensions: ['', '.js', '.jsx']
     },
     module: {
-        loaders: [
-            {
-                test: /\.js[x]?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
+        loaders: [{
+            test: /\.js[x]?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
+            }
 
-            },
-			{
-				test: /\.css$/,
-				loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-		    }
-        ]
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }]
     },
     plugins: [
-		new ExtractTextPlugin('public/css/[name].css'),
+        new ExtractTextPlugin('css/[name].css'),
         new webpack.HotModuleReplacementPlugin(),
-		new ConfiPlugin()
+        new ConfiPlugin()
     ],
-	
+
 };
 
 function ConfiPlugin() {}
 
 ConfiPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('emit', function(compilation, callback) {
-    var hash = compilation.hash.substr(0, 8);
-    var publicPath = prod ? '//static.seeyouyima.com/' + hash + '/' : 'http://127.0.0.1:8080/public/assets/';
-    var htmlMap = getHtml();
-    htmlMap.forEach(function(name) {
-      fs.readFile(name, 'utf-8', function(error, data) {
-        if (error) throw error;
-        var jsString = data.match(/\<script src=\".+\/js\/.+\.js.+\<\/script\>/g);
-        var cssString = data.match(/\<link type="text\/css" rel="stylesheet".+href=\".+\/css\/.+\.css.+\/\>/g);
-        var m = [];
-        var newString = '';
-        if(jsString) {
-          jsString.forEach(function(value) {
-            m = value.match(/\<script src=\".+\/js\/(.+)\.js/);
-            newString = '<script src="' + publicPath + 'js/' + m[1] + '.js"></script>';
-            var regExp = new RegExp(value);
-            data = data.replace(regExp, newString);
-          });
-        }
-        if(cssString) {
-          cssString.forEach(function(value) {
-            m = value.match(/\<link.+href=\".+\/css\/(.+)\.css/);
-            newString = '<link type="text/css" rel="stylesheet" href="' + publicPath + 'css/' + m[1] + '.css"/>';
-            var regExp = new RegExp(value);
-            data = data.replace(regExp, newString);
-          });          
-        }
-        fs.writeFile(name, data, 'utf-8', function(error) {
-          if (error) throw error;
-          console.log('Replace paths of ' + name + ' successfully ...');
+    compiler.plugin('emit', function(compilation, callback) {
+        var hash = compilation.hash.substr(0, 8);
+        var publicPath = prod ? '//static.seeyouyima.com/' + hash + '/' : 'http://127.0.0.1:8080/public/assets/';
+        var htmlMap = getHtml();
+        htmlMap.forEach(function(name) {
+            fs.readFile(name, 'utf-8', function(error, data) {
+                if (error) throw error;
+                var jsString = data.match(/\<script src=\".+\/js\/.+\.js.+\<\/script\>/g);
+                var cssString = data.match(/\<link type="text\/css" rel="stylesheet".+href=\".+\/css\/.+\.css.+\/\>/g);
+                var m = [];
+                var newString = '';
+                if (jsString) {
+                    jsString.forEach(function(value) {
+                        m = value.match(/\<script src=\".+\/js\/(.+)\.js/);
+                        newString = '<script src="' + publicPath + 'js/' + m[1] + '.js"></script>';
+                        var regExp = new RegExp(value);
+                        data = data.replace(regExp, newString);
+                    });
+                }
+                if (cssString) {
+                    cssString.forEach(function(value) {
+                        m = value.match(/\<link.+href=\".+\/css\/(.+)\.css/);
+                        newString = '<link type="text/css" rel="stylesheet" href="' + publicPath + 'css/' + m[1] + '.css"/>';
+                        var regExp = new RegExp(value);
+                        data = data.replace(regExp, newString);
+                    });
+                }
+                fs.writeFile(name, data, 'utf-8', function(error) {
+                    if (error) throw error;
+                    console.log('Replace paths of ' + name + ' successfully ...');
+                });
+            });
         });
-      });
+        callback();
     });
-    callback();
-  });
 };
 
 function getHtml() {
-  var htmlDir = config.src.views;
-  var names = fs.readdirSync(htmlDir);
-  var htmlMap = [];
-  names.forEach(function(name) {
-    var m = name.match(/.+\.html$/);
-    if(m) {
-      htmlMap.push(path.resolve('./views/' + name));
-    }    
-  });
-  return htmlMap;
+    var htmlDir = config.src.views;
+    var names = fs.readdirSync(htmlDir);
+    var htmlMap = [];
+    names.forEach(function(name) {
+        var m = name.match(/.+\.html$/);
+        if (m) {
+            htmlMap.push(path.resolve('./views/' + name));
+        }
+    });
+    return htmlMap;
 }
 
 module.exports = webpackConfig;
